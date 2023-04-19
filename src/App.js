4682c1e17bebd4cb1b8bbbc7363be970d524a66d
Rami-Ferjani <rami.ferjani3@gmail.com>
2023-04-19 22:20:27 +0100

    added another footer

--- src/App.js
+++ src/App.js
@@ -8,24 +8,37 @@ const App = () => {
   const [keywords, setKeywords] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(false);
-  const extractKeywords = (text) => {
+  const extractKeywords = async (text) => {
     setLoading(true);
     setIsOpen(true);
 
-    const options={
-      method: 'POST',
-      headers:{
-        'Content-Type':'application/json',
-        Authorization:`bearer ${import.meta.env.REACT_APP_KEY}`
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `bearer ${process.env.REACT_APP_KEY}`,
       },
-      body:JSON.stringify({
-        model:'text-davinci-003',
-        prompt:'Extract keywords from this text.Make the first letter of each word uppercase and separate with commas\n\n'+text+'',
-        temperature:0.5,
-        max_tokens:60,
-        frequency_penalty:0.8
-      })
-    }
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt:
+          "Extract keywords from this text.Make the first letter of each word uppercase and separate with commas\n\n" +
+          text +
+          "",
+        temperature: 0.5,
+        max_tokens: 60,
+        frequency_penalty: 0.8,
+      }),
+    };
+
+    const response = await fetch(process.env.REACT_APP_URL, options);
+
+    const json = await response.json();
+
+    const data = json.choices[0].text.trim();
+    console.log("hello");
+    console.log(data);
+    setKeywords(data);
+    setLoading(false);
   };
   return (
     <Box
@@ -38,6 +51,7 @@ const App = () => {
       <Container maxW="3xl" centerContent>
         <Header />
         <TextInput extractKeywords={extractKeywords} />
+        <Footer />
         <Footer />
       </Container>
     </Box>
